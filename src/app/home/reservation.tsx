d338777196225c311db61497ab0c2512f79fc682
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-05-17 09:33:32 -0500

    Searchcomponent

--- src/app/home/reservation.tsx
+++ src/app/home/reservation.tsx
@@ -1,8 +1,8 @@
-import Home from "@/modules/book/Home";
 import { Text, View } from "react-native";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
+import SearchComponent from "@/components/Search";
 export default function Reservation() {
     const { loading, isAuthenticated } = useAuth();
     const router = useRouter();
@@ -13,7 +13,9 @@ export default function Reservation() {
     }, [loading, isAuthenticated]);
     return (
         <View className="flex flex-1 ">
-            <Home />
+            <View className="mx-w-[700px] mx-h-[700px] bg-white rounded-lg shadow-md p-4">
+                <SearchComponent />
+            </View>
         </View>
     );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,30 @@
+import { View, Text } from "react-native";
+import { Search } from "lucide-react-native";
+import { Button } from "@/components/ui/button";
+import { Input, InputField } from "@/components/ui/input";
+import { useState } from "react";
+
+export default function SearchComponent() {
+    const [search, setSearch] = useState("");
+
+    const handleSearch = () => {
+        // Aquí puedes manejar la búsqueda
+        console.log("Buscando:", search);
+    };
+    return (
+        <View className="flex flex-row items-center justify-between px-4 py-2 bg-white border-b border-gray-200">
+            <Input
+                className="flex-1 p-1 rounded-md">
+                <InputField
+                    type="text"
+                    placeholder="Buscar..."
+                    value={search}
+                    onChangeText={setSearch}
+                />
+                <Button className="ml-2" variant="link" onPress={handleSearch} >
+                    <Search size={20} />
+                </Button>
+            </Input>
+        </View>
+    );
+}

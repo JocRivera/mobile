0128f9d90be1240177a41360403f04dd4717d75d
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-05-17 10:48:44 -0500

    FilterComponent

--- src/app/home/reservation.tsx
+++ src/app/home/reservation.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
 import SearchComponent from "@/components/Search";
+import Filter from "@/components/Filter";
 export default function Reservation() {
     const { loading, isAuthenticated } = useAuth();
     const router = useRouter();
@@ -15,6 +16,9 @@ export default function Reservation() {
         <View className="flex flex-1 ">
             <View className="mx-w-[700px] mx-h-[700px] bg-white rounded-lg shadow-md p-4">
                 <SearchComponent />
+            </View>
+            <View className="flex-1 px-4 py-6 md:px-6 ">
+                <Filter />
             </View>
         </View>
     );
---
+++ src/components/Filter.tsx
@@ -0,0 +1,30 @@
+import { Select, SelectTrigger, SelectInput, SelectIcon, SelectPortal, SelectBackdrop, SelectContent, SelectDragIndicator, SelectDragIndicatorWrapper, SelectItem } from "@/components/ui/select";
+import { ChevronDownIcon } from "lucide-react-native";
+import { View } from 'react-native'; // Importa View si aún no lo has hecho
+
+export default function Filter() {
+    return (
+        <Select>
+            <SelectTrigger
+                className="rounded-md border border-gray-300 py-2.5 px-3 flex-row items-center justify-between"
+                size="xl"
+            >
+                <SelectInput placeholder="Estado" />
+                <View className="ml-2"> {/* Ajusta el margen izquierdo del icono */}
+                    <ChevronDownIcon />
+                </View>
+            </SelectTrigger>
+            <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                    <SelectDragIndicatorWrapper>
+                        <SelectDragIndicator />
+                    </SelectDragIndicatorWrapper>
+                    <SelectItem label="Confirmada" value="Confirmada" />
+                    <SelectItem label="Pendiente" value="Pendiente" />
+                    <SelectItem label="Cancelada" value="Cancelada" />
+                </SelectContent>
+            </SelectPortal>
+        </Select>
+    );
+}

b03bfea30da871f7add0d8c170309176fccab004
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-05-17 17:56:09 -0500

    reservations

--- src/app/home/_layout.tsx
+++ src/app/home/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from 'expo-router';
+import { Home, CalendarDays } from 'lucide-react-native'; // importa los íconos
 
 export default function HomeLayout() {
     return (
@@ -9,7 +10,12 @@ export default function HomeLayout() {
                 headerShown: false,
             }}
         >
-            <Tabs.Screen name="reservation" options={{ title: 'Reservas' }} />
+            <Tabs.Screen name="reservation" options={{
+                title: 'Reservas',
+                tabBarIcon: ({ color, size }) => (
+                    <CalendarDays color={color} size={size} />
+                ),
+            }} />
             <Tabs.Screen name="accomodation" options={{ title: 'Alojamientos' }} />
         </Tabs>
     );
